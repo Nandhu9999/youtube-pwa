8cb8df1b8ccdce34349df6e1c899cdd282d7239b
Nandhu9999 <nandhakumar2058@gmail.com>
2024-10-07 08:49:07 +0530

    app icon

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Header from "./components/common/Header.tsx";
 function App() {
   return (
     <div className="flex h-screen flex-col items-center justify-center bg-[#f1d4b3] font-sans">
-      <h1 className="text-lg font-bold">Youtube PWA <span className="text-sm font-normal">v1.0.0</span></h1>
+      <h1 className="text-lg font-bold">Youtube PWA <span className="text-sm font-normal">v1.1.0</span></h1>
       <main className="relative grid h-[95%] max-h-full w-[95%] grid-rows-[64px_7fr] overflow-hidden rounded-[8px] bg-[#fff] md:h-[636px] md:w-[972px]">
         <Header />
         <Outlet />
